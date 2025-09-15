04fe559a13648c2ee284d08b38353082b3e84c97
Brando Endona <cbrandoendona@gmail.com>
2025-09-15 16:32:16 +0800

    update logger format

--- pkg/logger/v1/logger.go
+++ pkg/logger/v1/logger.go
@@ -1,51 +1,66 @@
 package logger
 
 import (
-	"errors"
-	"log"
-	"syscall"
+	"bytes"
+	"encoding/json"
+	"os"
+	"time"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/buffer"
 	"go.uber.org/zap/zapcore"
 )
 
-type Logger struct {
-	*zap.SugaredLogger
+func New() (*zap.SugaredLogger, error) {
+	encoderCfg := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     customTimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	baseEncoder := zapcore.NewJSONEncoder(encoderCfg)
+	prettyCore := zapcore.NewCore(&prettyJSONEncoder{baseEncoder}, zapcore.AddSync(os.Stdout), zapcore.DebugLevel)
+
+	logger := zap.New(prettyCore, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+	return logger.Sugar(), nil
 }
 
-type Config struct {
-	LogLevel string
+func customTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05"))
 }
 
-func NewLogger(config *Config) (*Logger, error) {
-	var level zapcore.Level
+type prettyJSONEncoder struct {
+	zapcore.Encoder
+}
 
-	err := level.Set(config.LogLevel)
+func (p *prettyJSONEncoder) EncodeEntry(ent zapcore.Entry, fields []zapcore.Field) (*buffer.Buffer, error) {
+	buf, err := p.Encoder.EncodeEntry(ent, fields)
 	if err != nil {
 		return nil, err
 	}
 
-	zapConfig := zap.NewProductionConfig()
-	zapConfig.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	zapConfig.Level.SetLevel(level)
-
-	zapLogger, err := zapConfig.Build()
-	if err != nil {
-		return nil, err
+	var raw map[string]interface{}
+	if err := json.Unmarshal(buf.Bytes(), &raw); err != nil {
+		return buf, nil
 	}
 
-	return &Logger{zapLogger.Sugar()}, nil
-}
-
-func (l *Logger) Sync() {
-	err := l.SugaredLogger.Sync()
-	if err == nil {
-		return
+	var pretty bytes.Buffer
+	enc := json.NewEncoder(&pretty)
+	enc.SetIndent("", "    ")
+	if err := enc.Encode(raw); err != nil {
+		return buf, nil
 	}
 
-	if errors.Is(err, syscall.EINVAL) {
-		return
-	}
-
-	log.Printf("Failed to sync logger: %v", err)
+	finalBuf := buffer.NewPool().Get()
+	finalBuf.AppendString(pretty.String())
+	buf.Free()
+	return finalBuf, nil
 }
