8e2120aac6e38a88692846caf8de60c9f12c16e8
Brando Endona <cbrandoendona@gmail.com>
2025-09-16 22:45:21 +0800

    create web app

---
+++ microservice/web_app/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+
+
+  return (
+    <div className="h-[40rem] w-full bg-white relative flex flex-col items-center justify-center antialiased">
+
+    </div>
+  );
+}
---
+++ microservice/web_app/src/constants.ts
@@ -0,0 +1,2 @@
+export const MINIMUM_IMAGE_SIZE = 1024 // 1kb
+export const MAXIMUM_IMAGE_SIZE = 1024 * 1024 * 10 // 10mb
