7d2e97205f1acefad7414d40dbc644eda9fac014
Brando Endona <cbrandoendona@gmail.com>
2025-09-17 10:35:33 +0800

    update docker-compose and create a dev.Dockerfile for web app

--- microservice/web_app/vite.config.ts
+++ microservice/web_app/vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+  },
 })
