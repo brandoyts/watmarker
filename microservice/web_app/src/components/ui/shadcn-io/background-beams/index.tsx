72f74c6a82eff785f195a6aef3afee9957be12ed
Brando Endona <cbrandoendona@gmail.com>
2025-09-18 08:56:49 +0800

    fix linter errors

--- microservice/web_app/src/components/ui/shadcn-io/background-beams/index.tsx
+++ microservice/web_app/src/components/ui/shadcn-io/background-beams/index.tsx
@@ -96,7 +96,7 @@ export const BackgroundBeams = React.memo(
           ))}
 
           <defs>
-            {paths.map((path, index) => (
+            {paths.map((_, index) => (
               <motion.linearGradient
                 id={`linearGradient-${index}`}
                 key={`gradient-${index}`}
